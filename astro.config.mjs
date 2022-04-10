5c210136f49110caa924b61a29fa7c0a9ae88b5b
Pukimaa <pukima@pukima.site>
2022-04-10 16:58:09 +0200

    Adding site variable in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 export default defineConfig({
+  site: "https://pukima.site",
   integrations: [
     react(),
   ],
