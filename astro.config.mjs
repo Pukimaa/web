7280138b7756a4efdf3e2b55e5e4f31e70313b4b
Pukimaa <pukima@pukima.site>
2022-04-10 17:27:23 +0200

    Add turbolinks (thanks to Hippo for pointing it out)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import turbolinks from '@astrojs/turbolinks';
+
 export default defineConfig({
   site: "https://pukima.site",
   integrations: [
     react(),
+    turbolinks(),
   ],
   vite: {
     ssr: {
