f36588c2e79126da732921116b14e0e68a2c9708
Pukimaa <pukima@pukima.site>
2022-04-11 23:18:18 +0200

    Added sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import turbolinks from "@astrojs/turbolinks";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://pukima.site",
-  integrations: [react(), turbolinks()],
+  integrations: [react(), turbolinks(), sitemap()],
   vite: {
     ssr: {
       external: ["svgo"],
