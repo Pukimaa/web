7147dc6e3d56cabaf7f5a804e2792b7ebf3b52a1
Pukimaa <pukima@pukima.site>
2022-07-31 23:57:11 +0200

    Update some packages and better layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://pukima.site",
   integrations: [react(), sitemap()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
