c6a66b548e9bb029ca360ef30d819c8dddd4da89
Pukimaa <pukima@pukima.site>
2022-08-09 15:38:56 +0200

    🎨 Prettify

--- public/swup.js
+++ public/swup.js
@@ -1,7 +1,7 @@
-import Swup from 'swup'
-import SwupA11yPlugin from '@swup/a11y-plugin'
-import SwupHeadPlugin from '@swup/head-plugin'
+import Swup from "swup";
+import SwupA11yPlugin from "@swup/a11y-plugin";
+import SwupHeadPlugin from "@swup/head-plugin";
 
 const swup = new Swup({
-    plugins: [new SwupA11yPlugin(), new SwupHeadPlugin()]
-})
+  plugins: [new SwupA11yPlugin(), new SwupHeadPlugin()],
+});
